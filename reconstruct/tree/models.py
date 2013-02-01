aef5cfa0e4e3c2637ed0a0ac0dcd1e7e0f68c775
darmar <darmar@darmar.(none)>
2013-02-01 20:58:05 +0100

    new models

--- reconstruct/tree/models.py
+++ reconstruct/tree/models.py
@@ -1,12 +1,30 @@
 from django.db import models
+from sorl.thumbnail.fields import ImageField
+# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=255)
+    # pieces = ...
+    def __unicode__(self):
+        return self.name
 
 
 class Family(models.Model):
-	surname = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=255, blank=True, null=True,)
+    footnote = models.CharField(max_length=255, blank=True, null=True,)
+    story = models.TextField(blank=True, null=True,)
+    sound = models.FileField(null=True, upload_to="zvuks", blank=True, )
+    countries = models.ManyToManyField("Country", blank = True, null=True,)  
+    def __unicode__(self):
+        return self.prezime  
 
-	def __unicode__(self):
-		return self.surname
+
+class Pic(models.Model):
+    family=models.ForeignKey("Family", null=True, related_name="pics", blank=True)
+    decade= models.IntegerField(null=True, blank=True)
+    image=ImageField(null=True, upload_to="slike", blank=True)
+    def __unicode__(self):
+        return self.image
 
 
-	class Meta:
-		verbose_name_plural = 'Families'
+
