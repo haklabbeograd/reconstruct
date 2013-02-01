a5577acebf94fc6d0c011c29a12accf344923c51
Vladan Popovic <vladanovic@gmail.com>
2013-02-01 21:30:16 +0100

    fix spacing and indentation in models
    add get_absolute_url in Family model

--- reconstruct/tree/models.py
+++ reconstruct/tree/models.py
@@ -1,30 +1,40 @@
+from django.conf import settings
 from django.db import models
 from sorl.thumbnail.fields import ImageField
-# Create your models here.
+
+
 
 class Country(models.Model):
     name = models.CharField(max_length=255)
-    # pieces = ...
+
+
     def __unicode__(self):
         return self.name
 
 
+
 class Family(models.Model):
-    last_name = models.CharField(max_length=255, blank=True, null=True,)
-    footnote = models.CharField(max_length=255, blank=True, null=True,)
-    story = models.TextField(blank=True, null=True,)
-    sound = models.FileField(null=True, upload_to="zvuks", blank=True, )
-    countries = models.ManyToManyField("Country", blank = True, null=True,)  
+    last_name = models.CharField(max_length=255)
+    footnote = models.CharField(max_length=255, blank=True, null=True)
+    story = models.TextField(blank=True, null=True)
+    sound = models.FileField(upload_to="zvuks", blank=True, null=True)
+    countries = models.ManyToManyField(Country, blank=True, null=True)
+
+
+    def get_absolute_url(self):
+        return "/family/%d" % self.id
+
+
     def __unicode__(self):
-        return self.prezime  
+        return self.last_name
+
 
 
 class Pic(models.Model):
-    family=models.ForeignKey("Family", null=True, related_name="pics", blank=True)
-    decade= models.IntegerField(null=True, blank=True)
-    image=ImageField(null=True, upload_to="slike", blank=True)
+    family = models.ForeignKey(Family, related_name="pics", blank=True, null=True)
+    decade = models.IntegerField(blank=True, null=True)
+    image = ImageField(upload_to="slike", blank=True, null=True)
+
+
     def __unicode__(self):
-        return self.image
-
-
-
+        return self.image.url
