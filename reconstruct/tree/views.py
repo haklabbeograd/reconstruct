aa364f303b3a907c064bb877387a377b3c8ffe51
Vladan Popovic <vladanovic@gmail.com>
2013-02-01 21:31:49 +0100

    replace tabs with 4 spaces in views.py

--- reconstruct/tree/views.py
+++ reconstruct/tree/views.py
@@ -7,25 +7,25 @@ from reconstruct.tree.models import Family
 
 
 def home(request):
-	t = get_template("home.html")
-	c = Context({'name': 'Homer i nemirno More'})
-	return HttpResponse(t.render(c))
+    t = get_template("home.html")
+    c = Context({'name': 'Homer i nemirno More'})
+    return HttpResponse(t.render(c))
 
 
 
 def families(request):
-	families = Family.objects.all()
-	return render_to_response("families.html", {'families': families})
+    families = Family.objects.all()
+    return render_to_response("families.html", {'families': families})
 
 
 
 def show_family(request, family_id):
-	family = get_object_or_404(Family, pk=family_id)
+    family = get_object_or_404(Family, pk=family_id)
 
-	# get_object_or_404 is basicaly this:
-	# try:
-	# 	family = Family.objects.get(pk=family_id)
-	# except Family.DoesNotExist:
-	# 	raise Http404
+    # get_object_or_404 is basicaly this:
+    # try:
+    # 	family = Family.objects.get(pk=family_id)
+    # except Family.DoesNotExist:
+    # 	raise Http404
 
-	return render_to_response("family.html", {'family': family})
+    return render_to_response("family.html", {'family': family})
